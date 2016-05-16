cf12304a47cf6c54cff935eb6fe1481db6b3ba41
李胜营 <shengying@live.cn>
2016-05-16 11:02:27 +0800

    初始化项目

---
+++ json姐血/json姐血/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  json姐血
+//
+//  Created by 李胜营 on 16/5/16.
+//  Copyright © 2016年 dasheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
